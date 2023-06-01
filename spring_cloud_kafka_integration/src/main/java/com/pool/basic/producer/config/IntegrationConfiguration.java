c81eedda6db34da9e1fc580424cb5af339e3d727
shiva508 <dasarishiva1@gmail.com>
2023-06-01 18:29:59 +0530

    Kafka,ElasticSearch Integration

--- spring_cloud_kafka_integration/src/main/java/com/pool/basic/producer/config/IntegrationConfiguration.java
+++ spring_cloud_kafka_integration/src/main/java/com/pool/basic/producer/config/IntegrationConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.MessageChannels;
 import org.springframework.integration.kafka.dsl.Kafka;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -20,7 +19,7 @@ public class IntegrationConfiguration {
 	public IntegrationFlow integrationFlow(MessageChannel messageChannel, 
 										   KafkaTemplate<Object, Object> kafkaTemplate) {
 		var kafka = Kafka.outboundChannelAdapter(kafkaTemplate).topic(topicName).get();
-		return IntegrationFlows.from(messageChannel).handle(kafka).get();
+		return IntegrationFlow.from(messageChannel).handle(kafka).get();
 	}
 	
 	@Bean
